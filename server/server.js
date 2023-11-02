4e6e9620a4a4db1fd624eea191a26565bd187903
Christian Carlson <christiancarlson648@gmail.com>
2023-11-02 13:45:09 -0500

    Added functionality to the previously mentioned function

--- server/server.js
+++ server/server.js
@@ -21,11 +21,13 @@ app.post('/guesses', (req, res) => {
   console.log(req.body);
   let guesses = req.body;
   res.sendStatus(201);
+  let results = checkRound(guesses);
+  console.log(results);
 })
 
 function compareNumbers(playerNumber) {
   // Check if inputs are too high, low, or equal to number
-  if (playerNumber == currentNumber) {
+  if (playerNumber === currentNumber) {
     return 'is correct';
   } else if (playerNumber > currentNumber) {
     return 'is too high';
@@ -35,23 +37,13 @@ function compareNumbers(playerNumber) {
 }
 
 console.log(compareNumbers(1));
-// function checkRound(guesses) {
-//   if (guesses.marcos === currentNumber) {
-//     console.log(`Congrats Marcos! You got it!`)
-//   }
-//   if (guesses.christian === currentNumber) {
-//     console.log(`Congrats Christian! You got it!`)
-
-//   }
-//   if (guesses.evan === currentNumber) {
-//     console.log(`Congrats Evan! You got it!`)
-
-//   }
-//   if (guesses.jaden === currentNumber) {
-//     console.log(`Congrats Jaden! You got it!`)
-//   }
-//   return true;
-// }
+function checkRound(guesses) {
+  let result = {marcos:compareNumbers(Number(guesses.marcos)),
+    christian:compareNumbers(Number(guesses.christian)),
+    evan:compareNumbers(Number(guesses.evan)),
+    jaden:compareNumbers(Number(guesses.jaden))};
+  return result;
+}
 
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
